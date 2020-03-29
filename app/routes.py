797d329126d0567ad493e6f63ba9ef2333ca728b
VictorNguyenCS <VictorNguyenCS@gmail.com>
2020-03-29 16:37:07 -0500

    init

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from flask import render_template, flash, redirect, url_for, request, send_from_
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app import db
 
 @app.route('/')
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///app'
     #os.environ.get('DATABASE_URL') or \
     #   'sqlite:///app.db' #+ os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
